cd395c4bbf43ad93facae991cb0f1923ff74dd59
鲍雄伟 <imjerrybao@gmail.com>
2016-03-17 09:35:50 +0800

    Initial Commit

---
+++ CircleProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleProgress
+//
+//  Created by Jerry Bao on 3/17/16.
+//  Copyright © 2016 JerryInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CircleProgress/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CircleProgress
+//
+//  Created by Jerry Bao on 3/17/16.
+//  Copyright © 2016 JerryInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
